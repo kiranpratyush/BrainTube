d43098cfc695d6e8419c29357cb666751f8282e8
Pratyush kiran Rout <pratyushkiranrout@gmail.com>
2022-04-02 12:46:13 +0530

    Refactor : replace if else with ternary operatoe

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,35 +1,28 @@
 import React from 'react';
 import youtube from '../../Assets/youtube.svg';
-import "./Header.css"
+import './Header.css';
 import MenuSharpIcon from '@mui/icons-material/MenuSharp';
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
 import { useAuth } from '../../Contexts/Auth.context';
 import { useNavigate } from 'react-router-dom';
 export function Header() {
-  const {state,dispatch} = useAuth()
-  const navigate = useNavigate()
-  function handleNavigate()
-  {
-    if(!state.user){
-      navigate("/auth/login")
-    }
-    else
-    {
-      dispatch({type:"SIGN_OUT"})
-    }
+  const { state, dispatch } = useAuth();
+  const navigate = useNavigate();
+  function handleNavigate() {
+    !state.user ? navigate('/auth/login') : dispatch({ type: 'SIGN_OUT' });
   }
   return (
     <div className="header">
-      <div className='header__left'>
-      <MenuSharpIcon />
-      <img src={youtube} alt="" className="header__logo" />
+      <div className="header__left">
+        <MenuSharpIcon />
+        <img src={youtube} alt="" className="header__logo" />
       </div>
-      <div className='header__input'>
-      <input type="text" placeholder='Search' />
-      <SearchSharpIcon className='header__inputButton' />
+      <div className="header__input">
+        <input type="text" placeholder="Search" />
+        <SearchSharpIcon className="header__inputButton" />
       </div>
-      <button onClick ={handleNavigate}>
-         {state.user?"Log out":"Sign in"}
+      <button onClick={handleNavigate}>
+        {state.user ? 'Log out' : 'Sign in'}
       </button>
     </div>
   );
